9ad72742c1ff524793233d9f94e918210d67415f
Lu <lucha.m94@gmail.com>
2021-11-22 00:06:32 -0300

    added keyboard support, after making operations in chain and pressing '=' and trying to make another operation an erroneous result was shown, I fixed this. I had to change some functions that I originally wrote as arrow-functions, I also put a 12 character limit in the current part of the display.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const current = document.querySelector('.currentNum');
 let firstValue='';
 let secondValue='';
 let currentOp = null;
-//18
+
 numberBttn.forEach((numBttn) =>
     numBttn.addEventListener('click', () => appendNumber(numBttn.textContent))
 );
@@ -18,39 +18,47 @@ operationBttn.forEach((opBttn) => {
     opBttn.addEventListener('click', () => chooseOp(opBttn.textContent))
 });
 
-periodBttn.addEventListener('click', () =>{
+periodBttn.addEventListener('click', addPeriod());
+
+function addPeriod() {
     if (current.textContent == '') return
     if (current.textContent.includes('.')) return
     else current.textContent += '.';
-})
+}
 
 equalBttn.addEventListener('click', () => compute());
 
-ceBttn.addEventListener('click', () =>{
+ceBttn.addEventListener('click', clearEverything());
+
+function clearEverything() {
     current.textContent='';
     previous.textContent='';
     firstValue='';
     secondValue='';
     currentOp=null;
-});
+}
 
-delBttn.addEventListener('click', () =>{
+delBttn.addEventListener('click', deleteNum()); 
+
+function deleteNum(){
     current.textContent = current.textContent
     .toString()
     .slice(0,-1);
-})
+}
 
 function appendNumber(num){
+    if (current.textContent.length === 12) return
     current.textContent += num;
 }
 
 function chooseOp(op){
     if (current.textContent === '') return;
-    if (previous.textContent !== ''){compute()}
+    if ((previous.textContent !== '') && (!previous.textContent.includes ('='))) compute();
     currentOp = op;
     firstValue = current.textContent;
     previous.textContent = current.textContent + currentOp;
     current.textContent = ''; 
+    op = null;
 }
 
 function compute(){
@@ -59,6 +67,7 @@ function compute(){
         current.textContent = 'ERROR';
         return
     }
+    if (previous.textContent == '') return
     secondValue = current.textContent;
     previous.textContent = previous.textContent + current.textContent + ' = ';
     current.textContent = roundNumber(operate(currentOp, firstValue, secondValue));
@@ -78,14 +87,25 @@ function operate (op, a, b){
     if (op === '-'){
         res = a-b;
     } else 
-    if (op === '×') {
+    if (op === '×' || op === '*')  {
         res = a*b;
     } else
-    if (op === '÷'){
+    if (op === '÷' || op === '/'){
         res = a/b;
     } else
     if (op === '%'){
         res = a%b;
     } else return;
     return res;
+}
+
+window.addEventListener ('keydown', kbInput);
+
+function kbInput (e){
+    if (e.key >= 0 && e.key <= 9) appendNumber (e.key);
+    if (e.key == '+' || e.key == '-' || e.key == '%' || e.key == '*' || e.key == '/') chooseOp(e.key);
+    if (e.key == '=' || e.key == 'Enter') compute();
+    if (e.key == '.') addPeriod();
+    if (e.key == 'Backspace') deleteNum();
+    if (e.key == 'Escape' || e.key == 'Delete') clearEverything();
 }
